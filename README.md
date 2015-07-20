8c3aa0893180e901b548bc2a56c026b429aa4231
sanchezzzhak <sanchezzzhak@ya.ru>
2015-07-20 08:58:42 +0100

    select2 base

---
+++ README.md
@@ -0,0 +1,41 @@
+Select2 widgets
+================
+Select2 widgets for Yii2
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist kak/select2 "*"
+```
+
+or add
+
+```
+"kak/select2": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \kak\widgets\Select2::widget(); ?>
+```
+
+```php
+<?= $form->field($model, 'list')->widget('\kak\widgets\Select2', [
+    'items' => [],
+    'options' => [
+        'class' => 'myCssClass'
+    ],
+]) ?>
+```
---
+++ Select2.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace kak\widgets\select2;
+
+use yii\helpers\Html;
+use yii\base\InvalidConfigException;
+
+class Select2 extends \yii\widgets\InputWidget
+{
+    const THEME_DEFAULT = 'default';
+    const THEME_BOOTSTRAP = 'bootstrap';
+
+
+    /**
+     * @var array the HTML attributes for the widget container tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $options = [];
+    public $items = [];
+
+    public $multiple = false;
+    public $theme = self::THEME_BOOTSTRAP;
+
+    public function init()
+    {
+        $this->init();
+        $this->initDefaultOption();
+
+        // render input
+        echo $this->hasModel()
+            ? Html::activeDropDownList($this->model, $this->attribute, $this->items , $this->options)
+            : Html::dropDownList($this->name, $this->value, $this->items, $this->options);
+
+        $view = $this->getView();
+    }
+
+    protected function initDefaultOption()
+    {
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        }
+        if(!isset($this->options['multiple']))
+        {
+            $this->options['multiple'] = $this->multiple;
+        }
+    }
+
+}
