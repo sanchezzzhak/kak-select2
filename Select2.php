f8673d487fa1f8836dab22a5359a3b7b588cf036
sanchezzzhak <sanchezzzhak@ya.ru>
2015-08-05 13:10:11 +0100

    no message

--- Select2.php
+++ Select2.php
@@ -5,10 +5,12 @@ namespace kak\widgets\select2;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\base\InvalidConfigException;
+use yii\helpers\Json;
+use yii\helpers\Url;
 
 class Select2 extends \yii\widgets\InputWidget
 {
-    const THEME_DEFAULT = 'default';
+    const THEME_DEFAULT   = 'classic';
     const THEME_BOOTSTRAP = 'bootstrap';
 
 
@@ -21,55 +23,83 @@ class Select2 extends \yii\widgets\InputWidget
     /** @var array */
     public $options = [];
     /** @var array */
-
-    private $defaultClientOptions = [
-        'placeholder' => ''
-    ];
+    public $ajax;
+    public $tags;
+    public $multiple      = false;
+    public $theme         = self::THEME_BOOTSTRAP;
+    public $placeholder;
 
     public $clientOptions = [];
-
-
     /** @var array */
     public $items = [];
 
-    public $multiple = false;
-    public $theme    = self::THEME_BOOTSTRAP;
 
     public function init()
     {
-        $this->initDefaultOption();
+        parent::init();
+        $this->initOption();
+    }
 
+    public function run()
+    {
         // render input
         echo $this->hasModel()
             ? Html::activeDropDownList($this->model, $this->attribute, $this->items , $this->options)
             : Html::dropDownList($this->name, $this->value, $this->items, $this->options);
 
-        // asset attach
+        $this->registerAssets();
+    }
+
+    /**
+     * Registers Assets
+     */
+    public function registerAssets()
+    {
         $view = $this->getView();
         Select2Asset::register($view)->addLanguage($this->language);
         if($this->theme == self::THEME_BOOTSTRAP) {
             ThemeBootstrap::register($view);
         }
-        $view->registerJs("$('#{$this->options['id']}').select2({$this->clientOptions})");
+        $id = $this->options['id'];
+        $clientOptions = Json::encode($this->$this->clientOptions);
 
+        $view->registerJs("jQuery('#{$id}').select2({$clientOptions})");
     }
 
-    protected function initDefaultOption()
+
+    protected function initOption()
     {
         if (!isset($this->options['id'])) {
             $this->options['id'] = $this->getId();
         }
 
-        $this->clientOptions['theme'] = $this->theme;
-        $this->clientOptions = ArrayHelper::merge($this->defaultClientOptions, $this->clientOptions);
+        if ($this->multiple) {
+            $this->options['data-multiple'] = 'true';
+            $this->options['multiple'] = true;
+        }
+
+        if (isset($this->tags)) {
+            $this->options['data-tags'] = $this->tags;
+            $this->options['multiple'] = true;
+        }
+
+        if ($this->language)
+            $this->options['data-language'] = $this->language;
+
+        if (isset($this->ajax)) {
+            $this->options['data-ajax--url'] = Url::to($this->ajax);
+            $this->options['data-ajax--cache'] = 'true';
+        }
+
+        if (isset($this->placeholder))
+            $this->options['data-placeholder'] = $this->placeholder;
+        
+        $this->clientOptions['theme']    = $this->theme;
 
         Html::addCssStyle($this->options,['width'=>'100%'],false);
         Html::addCssClass($this->options,'select2 form-control');
 
-        if(!isset($this->options['multiple']))
-        {
-            $this->options['multiple'] = $this->multiple;
-        }
+
     }
 
 }
